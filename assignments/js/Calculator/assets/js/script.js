aeb4bfe442445dd9d57e35f9abc2bde9eece3072
dilshan27 <damithaadilshan@gmail.com>
2022-09-24 16:58:03 +0530

    update

---
+++ assignments/js/Calculator/assets/js/script.js
@@ -0,0 +1,6 @@
+$("#btn-0").click(function () {
+    $("#lbl-display").text($("#lbl-display").text()+$("#btn-0").text());
+});
+$("#btnClear").click(function () {
+    $("#lbl-display").text("");
+});
