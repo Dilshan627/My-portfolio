a0474d410e5cb0a106c214774f80d6199a63319b
dilshan27 <damithaadilshan@gmail.com>
2022-09-26 21:36:07 +0530

    update

--- assignments/js/Calculator/assets/js/script.js
+++ assignments/js/Calculator/assets/js/script.js
@@ -1,5 +1,4 @@
-var num1,num2,total,operator;
-
+var num,total;
 $('.clc').click(function () {
     $('#display').val($('#display').val() + $(this).val())
 });
@@ -7,38 +6,23 @@ $('.clear').click(function () {
     $('#display').val(' ');
 });
 
+
 $('.sum').click(function () {
-    num1 = $('#display').val();
-    operator = '+';
-    $('#display').val(' ');
+
 });
 
 $('.sub').click(function () {
-    num1 = $('#display').val();
-    operator = '-';
-    $('#display').val(' ');
+
 });
+
 $('.division').click(function () {
-    num1 = $('#display').val();
-    operator = '/';
-    $('#display').val(' ');
+
 });
 $('.multi').click(function () {
-    num1 = $('#display').val();
-    operator = '*';
-    $('#display').val(' ');
+
 });
 
-
-
 $('.finalResult').click(function () {
-    switch (operator){
-        case '+':$('#result').val(num1+num2);break;
-        case '-':$('#result').val(num1-num2);break;
-        case 'x':$('#result').val(num1*num2);break;
-        case '/':$('#result').val(num1/num2);break;
-    }
-
 
     console.log($('#display').val());
 });
