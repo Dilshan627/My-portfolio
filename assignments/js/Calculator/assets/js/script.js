7ee4842afdfaacddfb1cb67317ac2678282a876a
dilshan27 <damithaadilshan@gmail.com>
2022-09-27 08:27:16 +0530

    update

--- assignments/js/Calculator/assets/js/script.js
+++ assignments/js/Calculator/assets/js/script.js
@@ -1,30 +1,57 @@
-var num,total;
+var num1, num2, operator;
 $('.clc').click(function () {
     $('#display').val($('#display').val() + $(this).val())
 });
+
 $('.clear').click(function () {
     $('#display').val(' ');
 });
 
 
 $('.sum').click(function () {
+    num1 = $('#display').val();
+    operator = "+";
+    $('#display').val(' ');
 
 });
 
 $('.sub').click(function () {
+    num1 = $('#display').val();
+    operator = "-";
+    $('#display').val(' ');
 
 });
 
 $('.division').click(function () {
+    num1 = $('#display').val();
+    operator = "/";
+    $('#display').val(' ');
 
 });
+
 $('.multi').click(function () {
+    num1 = $('#display').val();
+    operator = "*";
+    $('#display').val(' ');
+
 
 });
 
 $('.finalResult').click(function () {
+    num2 = $('#display').val();
 
-    console.log($('#display').val());
+    finalResult();
 });
+
+function finalResult() {
+
+    switch (operator){
+
+        case "+":
+
+
+    }
+
+}
 
 
