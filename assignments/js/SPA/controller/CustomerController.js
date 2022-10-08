c5aee3b70b6bd8c94a6b2a958a3fe0b3f581e891
dilshan27 <damithaadilshan@gmail.com>
2022-10-08 09:33:24 +0530

    update

--- assignments/js/SPA/controller/CustomerController.js
+++ assignments/js/SPA/controller/CustomerController.js
@@ -1,47 +1,5 @@
-$("#btnSaveCustomer").click(function () {
 
-    let cusId = $("#txtCusID").val();
-    let cusName = $("#txtCusName").val();
-    let cusAddress = $("#txtCusAddress").val();
-    let cusPhone = $("#txtCusPn").val();
 
-    var customer = {
-        "id": cusId,
-        "name": cusName,
-        "address": cusAddress,
-        "number": cusPhone
-    }
-
-    // console.log(customer);
-    customers.push(customer);
-    loadCustomer();
-    clickEvent();
-});
-
-function loadCustomer() {
-    $("#tblCustomer").empty();
-
-    for (var customer of customers) {
-
-        var row = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.number}</td></tr>`;
-
-        $("#tblCustomer").append(row);
-    }
-}
-
-function clickEvent() {
-    $('#tblCustomer>tr').click(function () {
-        let id = $(this).children(":eq(0)").text();
-        let name = $(this).children(":eq(1)").text();
-        let address = $(this).children(":eq(2)").text();
-        let number = $(this).children(":eq(3)").text();
-
-        $('#txtCusID').val(id);
-        $('#txtCusName').val(name);
-        $('#txtCusAddress').val(address);
-        $('#txtCusPn').val(number);
-    });
-}
 
 $('#txtCusID,#txtCusName,#txtCusName,#txtCusAddress').on('keydown',function (event) {
     if (event.key == "Tab") {
@@ -66,3 +24,50 @@ $('#txtCusAddress').on('keydown', function (event) {
         $('#txtCusPn').focus();
     }
 });
+
+function clickEvent() {
+    $('#tblCustomer>tr').click(function () {
+        let id = $(this).children(":eq(0)").text();
+        let name = $(this).children(":eq(1)").text();
+        let address = $(this).children(":eq(2)").text();
+        let number = $(this).children(":eq(3)").text();
+
+        $('#txtCusID').val(id);
+        $('#txtCusName').val(name);
+        $('#txtCusAddress').val(address);
+        $('#txtCusPn').val(number);
+    });
+}
+
+
+
+
+$("#btnSaveCustomer").click(function () {
+
+    let cusId = $("#txtCusID").val();
+    let cusName = $("#txtCusName").val();
+    let cusAddress = $("#txtCusAddress").val();
+    let cusPhone = $("#txtCusPn").val();
+
+    var customer = {
+        "id": cusId,
+        "name": cusName,
+        "address": cusAddress,
+        "number": cusPhone
+    }
+    customers.push(customer);
+    loadCustomer();
+    clickEvent();
+});
+
+function loadCustomer() {
+    $("#tblCustomer").empty();
+
+    for (var customer of customers) {
+
+        var row = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.number}</td></tr>`;
+
+        $("#tblCustomer").append(row);
+    }
+}
+
