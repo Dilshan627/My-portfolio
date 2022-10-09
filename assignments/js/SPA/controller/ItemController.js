5aefe8940a34afae6c5c229edf842a21d3e762a5
dilshan27 <damithaadilshan@gmail.com>
2022-10-09 09:56:13 +0530

    update

--- assignments/js/SPA/controller/ItemController.js
+++ assignments/js/SPA/controller/ItemController.js
@@ -6,8 +6,16 @@ var itemQtyOnHandRegEx = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
 /*array regex and filed add*/
 let itemValidations = [];
 itemValidations.push({reg: itemCodeRegEx, field: $('#txtItemCode'), error: 'Item ID Pattern is Wrong : I00-001'});
-itemValidations.push({reg: itemNameRegEx, field: $('#txtItemName'), error: 'Item Name is a required field : Minimum 3, Max 20, Spaces Allowed'});
-itemValidations.push({reg: itemUnitPriceRegEx, field: $('#txtItemQty'), error: 'Item Qty is a required field : Numbers'});
+itemValidations.push({
+    reg: itemNameRegEx,
+    field: $('#txtItemName'),
+    error: 'Item Name is a required field : Minimum 3, Max 20, Spaces Allowed'
+});
+itemValidations.push({
+    reg: itemUnitPriceRegEx,
+    field: $('#txtItemQty'),
+    error: 'Item Qty is a required field : Numbers'
+});
 itemValidations.push({reg: itemQtyOnHandRegEx, field: $('#txtItemPrice'), error: 'Item price / .00'});
 
 
@@ -47,12 +55,29 @@ $('#txtItemPrice').on('keydown', function (event) {
 });
 
 
-
 function itemCheckValidity() {
 
 }
 
-function itemCheck(regex, txtField){
+function itemCheck(regex, txtField) {
     let inputValue = txtField.val();
     return regex.test(inputValue) ? true : false;
+}
+
+function ItemClickEvent() {
+    $('#ItemTable>tr').click(function () {
+        let itemCode = $(this).children(":eq(0)").text();
+        let itemName = $(this).children(":eq(1)").text();
+        let price = $(this).children(":eq(2)").text();
+        let qty = $(this).children(":eq(3)").text();
+
+        ItemTextFieldValue(itemCode, itemName, price, qty);
+    });
+}
+
+function ItemTextFieldValue(itemCode, itemName, price, qty) {
+    $('#txtItemCode').val(itemCode);
+    $('#txtItemName').val(itemName);
+    $('#txtItemQty').val(price);
+    $('#txtItemPrice').val(qty);
 }
