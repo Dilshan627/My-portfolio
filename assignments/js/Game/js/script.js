aa22d2b91330caa1e18e0de34e1316934faca723
dilshan27 <damithaadilshan@gmail.com>
2022-11-13 17:23:52 +0530

    update

--- assignments/js/Game/js/script.js
+++ assignments/js/Game/js/script.js
@@ -9,12 +9,24 @@ let imagePositionX = 0;
 let marginTop = 660;
 
 window.addEventListener("load", function () {
+    // document.querySelector('.load').style.display = 'none';
+   /* animateManStart();
+    barriers();
+    ring();*/
+
+});
+document.getElementById("btnPlay").focus();
+document.getElementById("btnPlay").addEventListener("click", function () {
+    document.querySelector('.play').style.display = 'none';
+
     animateManStart();
     barriers();
     ring();
+
+    document.addEventListener("keypress", keyCheck);
 });
 
-document.addEventListener("keypress", keyCheck);
+
 var man = document.getElementById("character");
 
 
