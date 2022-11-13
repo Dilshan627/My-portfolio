160dc6f5027fabe9988359b018e3ec126b46bb55
dilshan27 <damithaadilshan@gmail.com>
2022-11-13 09:01:03 +0530

    run animation create

--- assignments/js/Game/js/script.js
+++ assignments/js/Game/js/script.js
@@ -7,12 +7,7 @@ let jumpImageNumber = 0;
 let jumpAnimationNumber = 0;
 
 document.addEventListener("keypress", keyCheck);
-document.addEventListener("keyup", clear);
 
-function clear(){
-    clearInterval(runAnimationNumber);
-    animateManStart();
-}
 
 function animateMan() {
     number = number + 1;
@@ -36,7 +31,7 @@ function runAnimation() {
 
 function runAnimationStart() {
     runAnimationNumber = setInterval(runAnimation, 120);
-    clearInterval( animateNumber);
+    clearInterval(animateNumber);
 }
 
 function jumpAnimation() {
@@ -56,11 +51,23 @@ function jumpAnimationStart() {
 function keyCheck(event) {
     var keyCode = event.which;
 
-    /*alert(event.which);*/
-
     if (keyCode == 13) {
         if (runAnimationNumber == 0) {
             runAnimationStart();
         }
+        if (moveBackgroundAnimation == 0) {
+            moveBackgroundAnimation=setInterval(moveBackground,100);
+        }
     }
+}
+
+let moveBackgroundAnimation = 0;
+
+let imagePositionX = 0;
+
+function moveBackground() {
+
+    imagePositionX = imagePositionX - 20;
+
+    document.getElementById("bg").style.backgroundPositionX = imagePositionX + "px";
 }
