88b7d1c13eba99177062f710f4dff4d3a8b5c69c
dilshan27 <damithaadilshan@gmail.com>
2022-11-20 10:36:42 +0530

    update

--- assignments/js/Game/js/script.js
+++ assignments/js/Game/js/script.js
@@ -1,30 +1,51 @@
-let number = 0;
-let animateNumber = 0;
-let runImageNumber = 0;
-let runAnimationNumber = 0;
-let jumpImageNumber = 1;
-let jumpAnimationNumber = 0;
-let moveBackgroundAnimation = 0;
-let imagePositionX = 0;
-let marginTop = 660;
-var marginLeftGo = 78;
-
-window.addEventListener("load", function () {
+/*window.addEventListener("load", function () {
     // document.querySelector('.load').style.display = 'none';
-    /* animateManStart();
+    /!* animateManStart();
      barriers();
-     ring();*/
+     ring();*!/
 
-});
+});*/
 
 document.getElementById("btnPlay").focus();
 
 document.getElementById("btnPlay").addEventListener("click", function () {
     document.querySelector('.play').style.display = 'none';
 
+    character1Start();
+    character2Start();
 
 });
 
 
-var man = document.getElementById("character1");
+var c1Number = 0;
+var c1startNumber;
+var c2Number = 0;
+var c2startNumber;
+
+var c1 = document.getElementById("character1");
+var c2 = document.getElementById("character2");
+
+function character1() {
+    c1Number = c1Number + 1;
+    if (c1Number == 10) {
+        c1Number = 0;
+    }
+    c1.src = "assets/ninjaadventurenew/png/Idle__00" + c1Number + ".png";
+}
+
+function character1Start() {
+    c1startNumber = setInterval(character1, 200);
+}
+
+function character2() {
+    c2Number = c2Number + 1;
+    if (c2Number == 10) {
+        c2Number = 0;
+    }
+    c2.src = "assets/ninjagirlnew/png/Idle__00" + c2Number + ".png";
+}
+
+function character2Start() {
+    c2startNumber = setInterval(character2, 200);
+}
 
